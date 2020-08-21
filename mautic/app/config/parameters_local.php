c058b5fac16ddfb1a563868042d32659e3608714
Jordan Ryan <jordan@facetinteractive.com>
2020-08-21 01:03:58 -0700

    Specify the exact IP from upstream

--- mautic/app/config/parameters_local.php
+++ mautic/app/config/parameters_local.php
@@ -282,6 +282,6 @@ Schedule a Meeting: https://calendly.com/facet-interactive',
 // Trusted Proxies are Required for Load Balanced Application Containers Behind a Proxy in Mautic
 // @TODO - Replace array with explode(",",getenv('TRUSTED_PROXIES'))
 $parameters['trusted_proxies'] = array(
-    '0' => '10.0.0.0/0',
+    '0' => '10.0.5.190/24',
     '1' => '172.16.0.0/12'
 );
