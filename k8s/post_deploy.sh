885b66685050990badda918707303837bb0e6e7a
Jordan Ryan <jordan@facetinteractive.com>
2020-09-30 23:51:52 -0700

    Mautic path updated

--- k8s/post_deploy.sh
+++ k8s/post_deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd /var/www/symfony/mautic
 app/console doctrine:migrations:migrate --no-ansi --no-interaction
 rm -rf /cache/{pro_,prod,run}
 until app/console cache:warmup ; do sleep 5; done; echo "OKAY"
