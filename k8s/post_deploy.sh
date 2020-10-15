e1891157113b1a85917f67c51fb9a1151738b08c
Jordan Ryan <jordan@facetinteractive.com>
2020-10-14 21:23:20 -0700

    - manually symlink .composer to make it writeable - change cache clear operations on deploy

--- k8s/post_deploy.sh
+++ k8s/post_deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 cd /var/www/symfony
-composer post-deploy
-rm -rf /cache/{pro_,prod,run}
+composer migrate
+composer installplugins
+mautic/app/composer cache:clear --env=prod
+# @TODO - re-enable, failing in production
+# rm -rf /cache/{pro_,prod,run}
 until mautic/app/console cache:warmup ; do sleep 5; done; echo "OKAY"
 
