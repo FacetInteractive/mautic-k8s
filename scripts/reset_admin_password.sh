793e8a28fcb06e800fa9a741309891a365c84e4d
Jordan Ryan <jordan@facetinteractive.com>
2021-01-11 17:42:39 -0800

    🧙‍♂️ - comment out redis in php.ini - correct ENV var for reset_admin_password - clean up whitespace in .lando.yml
    (cherry picked from commit 267b3500329873c9a4811ca43e140d8b28d5284c)

--- scripts/reset_admin_password.sh
+++ scripts/reset_admin_password.sh
@@ -9,7 +9,7 @@ cd "$(dirname "$0")/.."
 # HOST may not be necessary since this is running locally in lando container.
 mysql -h ${MYSQL_DB_HOST} \
       -u ${MYSQL_DB_USER} \
-      -p${MYSQL_DB_PASS} <<EOF
+      -p${MYSQL_DB_PASSWORD} <<EOF
 UPDATE ${MYSQL_DATABASE}.users SET password = "$2a$04$LrQYZmEMFi7GghF0EIv4FOdNv8bFcnlXM9Bta0eb8BWLLlRwcKrUm" where id = 1;
 EOF
 
